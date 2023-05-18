f8d534e3cfd00f2ee25e46d459d14747ca406d10
Jaival Saija <saijajaival@gmail.com>
2023-05-18 15:22:36 +0530

    Updated Schema to use next-auth.
    Packages added next-auth, prisma, prisma/client, and prisma-adapter.

--- env.mjs
+++ env.mjs
@@ -3,14 +3,14 @@ import { z } from 'zod';
 
 export const env = createEnv({
   server: {
-    // DATABASE_URL: z.string().url(),
+    DATABASE_URL: z.string().url(),
     // OPEN_AI_API_KEY: z.string().min(1),
   },
   client: {
     // NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
   },
   runtimeEnv: {
-    // DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
     // OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
     // NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
   },
